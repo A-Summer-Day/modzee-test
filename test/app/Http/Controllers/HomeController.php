935d59d4177cac5c66504dbf12c3e03ffcd1e096
Ha Le <ha@myseosucks.com>
2021-02-15 06:01:02 -0500

    Documentation

--- test/app/Http/Controllers/HomeController.php
+++ test/app/Http/Controllers/HomeController.php
@@ -16,13 +16,15 @@ class HomeController extends Controller
      */
     public function index()
     {
+		/* Since there is only 1 user record in the database, we fetch it with first
+		If there are more than 1, we can twist this data fetch easily by providing either the id of the user, or if we have authentication implemented, we can just fetch that id diectly from auth()->user() */
 		$user = User::with('images')->first();
 		
 		return response()->view('gallery',['user' => $user]);
 	}
 	
 	/**
-     * Display a listing of the resource.
+     * Display a listing of the resource, cararousel view.
      *
      * @return \Illuminate\Http\Response
      */
@@ -34,7 +36,7 @@ class HomeController extends Controller
 	}
 	
 	/**
-     * Display a listing of the resource.
+     * Display a listing of the resource, fancybox view.
      *
      * @return \Illuminate\Http\Response
      */
